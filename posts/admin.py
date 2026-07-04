b7991ad191ef3efaa54b9ab2bfb5e149022cddf9
nawabsakib5 <nawabsakib5@gmail.com>
2026-07-05 03:11:06 +0600

    Integration

--- posts/admin.py
+++ posts/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from .models import Post
 
-admin.site.register(Post)
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('social_account', 'status', 'scheduled_time', 'platform_post_id', 'created_at')
+    fields = ('social_account', 'content', 'media_file', 'scheduled_time', 'status', 'platform_post_id', 'error_message', 'created_by')
+    readonly_fields = ('platform_post_id', 'error_message')
